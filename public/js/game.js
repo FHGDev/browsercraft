c9806cd11d058be2335c0a1200afb2c74bac4289
omgimanerd <alvin.lin.dev@gmail.com>
2016-03-14 17:41:58 -0400

    Implemented basics of canvas on game.html

--- public/js/game.js
+++ public/js/game.js
@@ -5,7 +5,5 @@
 
 $(document).ready(function() {
   Input.applyEventHandlers();
-  Input.addMouseTracker(document.getElementById('game-canvas'));
-
-
+  Input.addMouseTracker(document.getElementById('game-canvas'), 'canvas');
 });
